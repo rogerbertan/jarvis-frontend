ba2ce177f8dcc1f58b8c0c9cfc7d07f29c99bc59
Roger Bertan <roger.bertan@gmail.com>
2025-10-26 14:31:20 -0300

    feat: implement middlewere to redirect public and private routes

---
+++ src/middleware.ts
@@ -0,0 +1,52 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+
+const publicRoutes = [
+  { path: "/login", whenAuthenticated: "redirect" },
+  { path: "/register", whenAuthenticated: "redirect" },
+] as const;
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = "/login";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const publicRoute = publicRoutes.find((route) => route.path === path);
+  const authToken = request.cookies.get("token");
+
+  if (!authToken && publicRoute) {
+    return NextResponse.next();
+  }
+
+  if (!authToken && !publicRoute) {
+    const redirectUrl = request.nextUrl.clone();
+
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE;
+
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (
+    authToken &&
+    publicRoute &&
+    publicRoute.whenAuthenticated === "redirect"
+  ) {
+    const redirectUrl = request.nextUrl.clone();
+
+    redirectUrl.pathname = "/";
+
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (authToken && !publicRoute) {
+    //TODO: testar se token esta expirado (se SIM, remove o cookie e redireciona para login)
+
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
